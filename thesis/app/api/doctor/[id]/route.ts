d525d8a4a027076a07eeac7c8306df95d1a02582
yassinifguisse1 <36793818+yassinifguisse1@users.noreply.github.com>
2024-12-18 16:44:58 +0100

    Update Doctor API endpoint whith Adding a Delete single Doctor by Id

--- thesis/app/api/doctor/[id]/route.ts
+++ thesis/app/api/doctor/[id]/route.ts
@@ -1,145 +1,185 @@
-import {  getAuth } from "@clerk/nextjs/server";
+import { getAuth } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/db";
 import { checkRole } from "@/utils/roles";
-import {  updateFormSchema } from "@/lib/shema";
-  
+import { updateFormSchema } from "@/lib/shema";
+import { redirect } from "next/navigation";
+
 interface Proptype {
-    params: { id: string };
-  }
-  
-  /**
+  params: { id: string };
+}
+
+/**
  * @method PUT
  * @route ~/api/doctor/:id
- * @desc Update Single Articles by id
+ * @desc Update Single doctor by id
  * @access public
  **/
 
-  export async function PUT(request: NextRequest, { params }: Proptype) {
-    try {
-       const { userId } = getAuth(request);
-          console.log("Request userId:", userId);
-          // console.log("Request Headers:", request.headers);
-      
-          if (!userId) {
-            return NextResponse.json(
-              { error: "User is not signed in." },
-              { status: 401 }
-            );
-          }
-      
-          const isAdmin = await checkRole("admin"); // Assuming checkRole takes userId and role
-          if (!isAdmin) {
-            // redirect('/')
-            return NextResponse.json(
-              { message: "Access denied. Admins only." },
-              { status: 403 }
-            );
-          }
-      const doctor = await prisma.doctor.findUnique({
+export async function PUT(request: NextRequest, { params }: Proptype) {
+  try {
+    const { userId } = getAuth(request);
+    console.log("Request userId:", userId);
+    // console.log("Request Headers:", request.headers);
 
-        where: {
-            id: params.id,
-        }
-      });
-      if (!doctor) {
-        return NextResponse.json(
-          { message: "doctor not fund" },
-          { status: 404 }
-        );
-      }
-      const body = await request.json();
-      const validation = updateFormSchema.safeParse(body);
-      if (!validation.success) {
-        return NextResponse.json({ error: "Invalid form data" }, { status: 400 });
-      }
-      const {
-            name,
-            speciality,
-            degree,
-            experience,
-            about,
-            fees,
-            image,
-            addressLine1,
-            addressLine2,
-          } = body;
-       
-          console.log("Validation:", validation);
-          console.log("Body:", body);
-          
-      const updatedDoctor = await prisma.doctor.update({
-        where: {
-            id: params.id,  
-        },
-        data: {
-          name,
-          speciality,
-          degree,
-          experience,
-          about,
-          fees,
-          image,
-          addressLine1,
-          addressLine2,
-        },
- 
-      })
-      return NextResponse.json( { message: "Doctor Updated successfully!", updatedDoctor },{ status: 200 });
-
-    } catch (error) {
-        console.log(error)
+    if (!userId) {
       return NextResponse.json(
-        { message: "internal server error" },
-        { status: 500 }
+        { error: "User is not signed in." },
+        { status: 401 }
       );
     }
-  }
 
-    /**
+    const isAdmin = await checkRole("admin"); // Assuming checkRole takes userId and role
+    if (!isAdmin) {
+      // redirect('/')
+      return NextResponse.json(
+        { message: "Access denied. Admins only." },
+        { status: 403 }
+      );
+    }
+    const doctor = await prisma.doctor.findUnique({
+      where: {
+        id: params.id,
+      },
+    });
+    if (!doctor) {
+      return NextResponse.json({ message: "doctor not fund" }, { status: 404 });
+    }
+    const body = await request.json();
+    const validation = updateFormSchema.safeParse(body);
+    if (!validation.success) {
+      return NextResponse.json({ error: "Invalid form data" }, { status: 400 });
+    }
+    const {
+      name,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      image,
+      addressLine1,
+      addressLine2,
+    } = body;
+
+    console.log("Validation:", validation);
+    console.log("Body:", body);
+
+    const updatedDoctor = await prisma.doctor.update({
+      where: {
+        id: params.id,
+      },
+      data: {
+        name,
+        speciality,
+        degree,
+        experience,
+        about,
+        fees,
+        image,
+        addressLine1,
+        addressLine2,
+      },
+    });
+    return NextResponse.json(
+      { message: "Doctor Updated successfully!", updatedDoctor },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: "internal server error" },
+      { status: 500 }
+    );
+  }
+}
+
+/**
  * @method GET
  * @route ~/api/doctor/:id
  * @desc GET Single Doctor by id
  * @access public
  **/
 
-  export async function GET(request: NextRequest,{ params }: Proptype) {
-    console.log("Received params:", params);
+export async function GET(request: NextRequest, { params }: Proptype) {
+  console.log("Received params:", params);
 
-    try {
-      const { userId } = getAuth(request);
-      console.log("Request userId:", userId);
-  
-      if (!userId) {
-        return NextResponse.json(
-          { error: "User is not signed in." },
-          { status: 401 }
-        );
-      }
-  
-     
-      const doctor = await prisma.doctor.findUnique(
-        {
-          where: {
-            id: params.id,
-          }
-        }
-      )
-      if (!doctor) {
-        return NextResponse.json(
-          { message: "doctor not fund" },
-          { status: 404 }
-        );
-      }
-      return NextResponse.json(doctor, { status: 200 });
-      
-    } catch (error) {
-      console.log("Eror thisi is ",error)
+  try {
+    const { userId } = getAuth(request);
+    console.log("Request userId:", userId);
+
+    if (!userId) {
       return NextResponse.json(
-        { message: "internal server errorb" },
-        { status: 500 }
-        );
-      
+        { error: "User is not signed in." },
+        { status: 401 }
+      );
     }
-    
+
+    const doctor = await prisma.doctor.findUnique({
+      where: {
+        id: params.id,
+      },
+    });
+    if (!doctor) {
+      return NextResponse.json({ message: "doctor not fund" }, { status: 404 });
+    }
+    return NextResponse.json(doctor, { status: 200 });
+  } catch (error) {
+    console.log("Eror thisi is ", error);
+    return NextResponse.json(
+      { message: "internal server errorb" },
+      { status: 500 }
+    );
   }
+}
+
+/**
+ * @method DELETE
+ * @route ~/api/doctor/:id
+ * @desc Delete Single Doctorz by id
+ * @access public
+ **/
+export async function DELETE(request: NextRequest, { params }: Proptype) {
+  try {
+    console.log("Received params:", params);
+    const { userId } = getAuth(request);
+    if (!userId) {
+      redirect("/sign-in");
+    }
+    // check if the admin role
+    const isAdmin = await checkRole("admin"); // Assuming checkRole takes userId and role
+    if (!isAdmin) {
+      // redirect('/')
+      return NextResponse.json(
+        { message: "Access denied. Admins only." },
+        { status: 403 }
+      );
+    }
+    // Check if the doctor exists before attempting to delete
+    const doctorExists = await prisma.doctor.findUnique({
+      where: {
+        id: params.id,
+      },
+    });
+
+    if (!doctorExists) {
+      return NextResponse.json(
+        { message: "Doctor not found" },
+        { status: 404 }
+      );
+    }
+     // Proceed to delete the doctor
+     await prisma.doctor.delete({
+      where: {
+        id: params.id,
+      },
+    });
+    return NextResponse.json({ message: "Doctor deleted" }, { status: 200 });
+  } catch (error) {
+    console.log("Error this is ", error);
+    return NextResponse.json(
+      { message: "internal server error" },
+      { status: 500 }
+    );
+  }
+}
