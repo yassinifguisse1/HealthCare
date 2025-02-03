003ed31b76b7b77ea9818dba5c3d226e523359ce
yassinifguisse1 <36793818+yassinifguisse1@users.noreply.github.com>
2025-02-03 16:17:41 +0100

    Refactor AdminSidebar component to support collapsible functionality; update sidebar styling and improve layout for better user experience.

--- thesis/app/admin/_components/AdminSidebar.tsx
+++ thesis/app/admin/_components/AdminSidebar.tsx
@@ -1,7 +1,7 @@
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { ScrollArea } from "@/components/ui/scroll-area"
-import { Sidebar, SidebarContent, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar"
+import { Sidebar, SidebarContent, SidebarHeader, SidebarMenu, SidebarMenuAction, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar"
 import { LayoutDashboard, Users, Settings, HelpCircle } from 'lucide-react'
 import Link from "next/link"
 import Image from 'next/image'
@@ -9,22 +9,16 @@ import logoImage from "@/assets/images/logosaas.png"
 
 export function AdminSidebar({ className }: React.HTMLAttributes<HTMLDivElement>) {
   return (
-    <Sidebar className={cn("border-r inset-0 bg-[#D3D3D3] text-black rounded-r-lg", className)}>
+    <Sidebar className={cn("border-r inset-0 bg-[#D3D3D3] text-black rounded-r-lg", className)} collapsible="icon">
       <SidebarHeader>
-         <div className="relative  flex items-center justify-center mx-auto size-12">
+         <div className="relative  flex items-center justify-center mx-auto size-12 group-data-[collapsible=icon]:hidden">
               <div className="absolute  w-full h-full top-0 left-0 bg-[linear-gradient(to_right,#F87BFF,#FB92CF,#FFDD9B,#C2F0B1,#2FD8FE)] blur-md opacity-75"></div>
               <Image
                 src={logoImage}
                 alt="Logo"
                 className="size-8 relative flex items-start justify-start"
               />
-              
-              
             </div>
-            <Button variant="ghost" className="w-full justify-start">
-          <LayoutDashboard className="mr-2 h-4 w-4" />
-          Admin Dashboard
-        </Button>
       </SidebarHeader>
       <SidebarContent>
         <ScrollArea className="h-[calc(100vh-8rem)] px-2">
@@ -36,6 +30,7 @@ export function AdminSidebar({ className }: React.HTMLAttributes<HTMLDivElement>
                   Overview
                 </Link>
               </SidebarMenuButton>
+              {/* <SidebarMenuAction className="peer-data-[active=true]/menu-button:opacity-100" /> */}
             </SidebarMenuItem>
             <SidebarMenuItem>
               <SidebarMenuButton asChild>
