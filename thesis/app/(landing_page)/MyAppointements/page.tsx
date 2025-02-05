ca9659a96029c6d0b64384aeb7ed0b61014a0c5b
yassinifguisse1 <36793818+yassinifguisse1@users.noreply.github.com>
2025-02-05 20:27:29 +0100

    Update error page styling and refactor API call to use relative path

--- thesis/app/(landing_page)/MyAppointements/page.tsx
+++ thesis/app/(landing_page)/MyAppointements/page.tsx
@@ -21,9 +21,9 @@ async function getAppointments() {
     const token = await getToken({ template: "TOKEN_Healthcare" });
 
     // Make sure we have a base URL
-    const baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000'
+    // const baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000'
     
-    const response = await axios.get(`${baseUrl}/api/appointments`, {
+    const response = await axios.get("/api/appointments", {
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${token}`,
